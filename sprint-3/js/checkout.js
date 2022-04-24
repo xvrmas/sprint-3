61abec2c6954c62843c0152cfd9f5efcd19c6c2b
Xavier Mas <xvrmasgarcia@gmail.com>
2022-04-24 19:08:51 +0200

    formulari bootstrap5

--- sprint-3/js/checkout.js
+++ sprint-3/js/checkout.js
@@ -9,6 +9,21 @@ var errorName = document.getElementById("errorName");
 var errorPhone = document.getElementById("errorPhone");
 
 // Exercise 6
-function validate() {
-  // Validate fields entered by the user: name, phone, password, and email
+// Validate fields entered by the user: name, phone, password, and email
+function validate(e) {
+  (function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 }
